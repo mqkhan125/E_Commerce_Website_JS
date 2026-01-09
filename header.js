cad98791901ea8e0d25a80c9913af4cd731c199a
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-01-09 12:00:04 +0500

    feat: add reusable header and footer components and integrate them across all pages

---
+++ header.js
@@ -0,0 +1,51 @@
+const headerHTML = `
+<header class="section-navbar">
+  <section class="top_txt">
+    <div class="head container">
+      <div class="head_txt">
+        <p>Free shipping, 30-day return or refund guarantee.</p>
+      </div>
+      <div class="sing_in_up">
+        <a href="#">SIGN IN</a>
+        <a href="#">SIGN UP</a>
+      </div>
+    </div>
+  </section>
+
+  <div class="container">
+    <div class="navbar-brand">
+      <a href="index.html">
+        <img src="./images/log.png" 
+         alt="qasim eCommerce logo" 
+         class="logo"
+         width="80%"
+         height="auto" />
+      </a>
+    </div>
+
+    <nav class="navbar">
+      <ul>
+        <li class="nav-item">
+          <a href="/" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="about.html" class="nav-link">about</a>
+        </li>
+        <li class="nav-item">
+          <a href="products.html" class="nav-link">products</a>
+        </li>
+        <li class="nav-item">
+          <a href="contact.html" class="nav-link">contact</a>
+        </li>
+        <li class="nav-item">
+          <a href="addToCart.html" class="nav-link add-to-cart-button" id="cartValue">
+            <i class="fa-solid fa-cart-shopping flex"> 0 </i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  </header>
+`
+  const headerElem = document.querySelector(".section-navbar");
+  headerElem.insertAdjacentHTML("afterbegin", headerHTML);
